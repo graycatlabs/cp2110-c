5da5935d459a640c03fa441f29fe0dcee1a561d7
Alex Hiam <alex@graycat.io>
2016-01-13 19:10:00 -0500

    regenerated docs

---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,33 @@
+var indexSectionsWithContent =
+{
+  0: "cdfps",
+  1: "c",
+  2: "c",
+  3: "c",
+  4: "c",
+  5: "dfps",
+  6: "c"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions",
+  3: "typedefs",
+  4: "enums",
+  5: "enumvalues",
+  6: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Functions",
+  3: "Typedefs",
+  4: "Enumerations",
+  5: "Enumerator",
+  6: "Pages"
+};
+
